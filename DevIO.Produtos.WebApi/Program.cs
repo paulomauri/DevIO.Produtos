a8c7a468df16db1c19410643088818bdf53b3c20
Paulo Mauri <paulo.mauri@gmail.com>
2025-06-25 15:29:09 -0300

    Ajustes Dockerile

--- DevIO.Produtos.WebApi/Program.cs
+++ DevIO.Produtos.WebApi/Program.cs
@@ -1,59 +1,13 @@
-//using DevIO.Produtos.Application.Command;
-//using DevIO.Produtos.Domain.Repository;
-//using DevIO.Produtos.Infrastructure.Data;
-//using DevIO.Produtos.Infrastructure.Repository;
-//using Microsoft.EntityFrameworkCore;
-//using Serilog;
-
-//Log.Logger = new LoggerConfiguration()
-//    .Enrich.FromLogContext()
-//    .WriteTo.Elasticsearch(new Serilog.Sinks.Elasticsearch.ElasticsearchSinkOptions(new Uri("http://elasticsearch:9200"))
-//    {
-//        AutoRegisterTemplate = true,
-//        IndexFormat = "produto-api-logs-{0:yyyy.MM.dd}"
-//    })
-//    .CreateLogger();
-
-//var builder = WebApplication.CreateBuilder(args);
-//builder.Host.UseSerilog();
-
-//// Add services to the container.
-
-//builder.Services.AddControllers();
-//builder.Services.AddDbContext<AppDbContext>(options =>
-//    options.UseInMemoryDatabase("ProdutosDb"));
-//builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(CreateProdutoCommand).Assembly));
-//builder.Services.AddScoped<IProdutoRepository, ProdutoRepository>();
-
-//// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-//builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen();
-
-//var app = builder.Build();
-
-//app.UseSwagger();
-//app.UseSwaggerUI();
-
-//app.UseHttpsRedirection();
-
-//app.UseAuthorization();
-
-//app.MapControllers();
-
-//app.Run();
-
 using DevIO.Produtos.Application.Command;
 using DevIO.Produtos.Domain.Repository;
 using DevIO.Produtos.Infrastructure.Data;
 using DevIO.Produtos.Infrastructure.Repository;
 using Microsoft.EntityFrameworkCore;
 using Serilog;
-using Serilog.Sinks.Elasticsearch;
 
 Log.Logger = new LoggerConfiguration()
     .Enrich.FromLogContext()
-    .WriteTo.Console()
-    .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri("http://elasticsearch:9200"))
+    .WriteTo.Elasticsearch(new Serilog.Sinks.Elasticsearch.ElasticsearchSinkOptions(new Uri("http://elasticsearch:9200"))
     {
         AutoRegisterTemplate = true,
         IndexFormat = "produto-api-logs-{0:yyyy.MM.dd}"
@@ -61,37 +15,85 @@ Log.Logger = new LoggerConfiguration()
     .CreateLogger();
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Faz o app escutar na porta 80 para funcionar corretamente no Docker
-builder.WebHost.UseUrls("http://0.0.0.0:80");
-
-// Usa Serilog
 builder.Host.UseSerilog();
 
-// Serviços
+// Add services to the container.
+
 builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseInMemoryDatabase("ProdutosDb"));
 builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(CreateProdutoCommand).Assembly));
 builder.Services.AddScoped<IProdutoRepository, ProdutoRepository>();
 
-// Swagger
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Swagger sempre ativado
 app.UseSwagger();
 app.UseSwaggerUI();
 
-// HTTPS redirection só fora do Docker
-if (!app.Environment.IsProduction())
-{
-    app.UseHttpsRedirection();
-}
+app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
 app.MapControllers();
+
 app.Run();
+
+// =============================== 
+
+//using DevIO.Produtos.Application.Command;
+//using DevIO.Produtos.Domain.Repository;
+//using DevIO.Produtos.Infrastructure.Data;
+//using DevIO.Produtos.Infrastructure.Repository;
+//using Microsoft.EntityFrameworkCore;
+//using Serilog;
+//using Serilog.Sinks.Elasticsearch;
+
+//Log.Logger = new LoggerConfiguration()
+//    .Enrich.FromLogContext()
+//    .WriteTo.Console()
+//    .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri("http://elasticsearch:9200"))
+//    {
+//        AutoRegisterTemplate = true,
+//        IndexFormat = "produto-api-logs-{0:yyyy.MM.dd}"
+//    })
+//    .CreateLogger();
+
+//var builder = WebApplication.CreateBuilder(args);
+
+//// Faz o app escutar na porta 80 para funcionar corretamente no Docker
+//builder.WebHost.UseUrls("http://0.0.0.0:80");
+
+//// Usa Serilog
+//builder.Host.UseSerilog();
+
+//// Serviços
+//builder.Services.AddControllers();
+//builder.Services.AddDbContext<AppDbContext>(options =>
+//    options.UseInMemoryDatabase("ProdutosDb"));
+//builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(CreateProdutoCommand).Assembly));
+//builder.Services.AddScoped<IProdutoRepository, ProdutoRepository>();
+
+//// Swagger
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
+
+//var app = builder.Build();
+
+//// Swagger sempre ativado
+//app.UseSwagger();
+//app.UseSwaggerUI();
+
+//// HTTPS redirection só fora do Docker
+//if (!app.Environment.IsProduction())
+//{
+//    app.UseHttpsRedirection();
+//}
+
+//app.UseAuthorization();
+//app.MapControllers();
+//app.Run();
 
